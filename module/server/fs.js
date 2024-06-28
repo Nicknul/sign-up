e00c0ab2ef1defa0d9f926557e6108f09d6d2ba2
Nicknul <jea7730@gmail.com>
2024-06-28 17:55:59 +0900

    edit_정호연 : 코드 수정

--- module/server/fs.js
+++ module/server/fs.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
 /**
  * * 24.06.28 (fri) 정호연
  * * fs 모듈화 작업
  * * fs 내장 모듈 검증 완료
  */
-const fs = {
-  fs: require('fs'),
+const fileSystemFu = {
+  read: (path, callback) => {
+    fs.readFile(`${path}`, (err, data) => {
+      if (err) {
+        console.error('서버 오류:', err);
+      } else {
+        callback(data);
+      }
+    });
+  },
 };
 
-console.log(fs);
+module.exports = fileSystemFu;
