fb4e73983c72960ce09f0008d57f58398e1fec7a
Nicknul <jea7730@gmail.com>
2024-06-28 16:01:49 +0900

    edit_정호연 : fs 모듈화
    require(fs)를 모듈화 작업_완
    검증 완료

--- module/server/fs.js
+++ module/server/fs.js
@@ -1,35 +1,10 @@
-const http = require('http');
-const fs = require('fs');
-
 /**
  * * 24.06.28 (fri) 정호연
- * * server fs 모듈화 작업
+ * * fs 모듈화 작업
+ * * fs 내장 모듈 검증 완료
  */
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    let data = fs.readFileSync('./public/html/index.html', 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(data);
-  }
-});
-
-server.listen(3000, (err) => {
-  if (err) {
-    console.error('서버 에러', err);
-  } else {
-    console.log('http://localhost:3000');
-  }
-});
-/**
- *
- * @param {*} type write, read
- * @param {*} path 파일 경로
- * @returns
- */
-const getCom = (type, path) => {
-  return `fs.${type}FileSyne(${path}, 'utf-8');`;
+const fs = {
+  fs: require('fs'),
 };
 
-const methodGET = getCom('read', './public/html/index.html');
-
-console.log(methodGET);
+console.log(fs);
