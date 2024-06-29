ce509da9258e501a63ca839c9c51b3a5ff1a9e87
Nicknul <jea7730@gmail.com>
2024-06-30 00:00:07 +0900

    edit_정호연 : 주석 수정

--- callback-1.js
+++ callback-1.js
@@ -1,7 +1,8 @@
 /**
  * * 24.06.29 (Sat) 정호연
- * @param {*} name 이름
- * @param {*} callback greet() 함수의 실행이 끝난 다음 실행 할 함수
+ * * callback() 테스트 1
+ * @param {string} name 이름
+ * @param {function} callback greet() 함수의 실행이 끝난 다음 실행 할 함수
  */
 const greet = (name, callback) => {
   console.log(`hello ${name}`);
