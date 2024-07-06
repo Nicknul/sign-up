cead926ec3e659105db444c01abb76e35266d90d
Nicknul <jea7730@gmail.com>
2024-07-06 22:20:52 +0900

    edit_정호연 : 주석 수정, box 불러오기

--- public/script/index.js
+++ public/script/index.js
@@ -1,9 +1,9 @@
-// tag, id 불러오기
+// * tag, id 불러오기
 const root = document.querySelector('#root');
 const form = document.querySelector('form');
 const names = document.querySelector('#name');
 
-// containers 불러오기
+// * container 불러오기
 const nameCon = document.querySelector('#name-container');
 const passwordCon = document.querySelector('#password-container');
 const passwordCheckCon = document.querySelector('#possword-check-container');
@@ -11,7 +11,15 @@ const emailCon = document.querySelector('#email-container');
 const phoneNumberCon = document.querySelector('#phone-number-container');
 // console.log(phoneNumberCon);
 
-// name Input 데이터베이스
+// * check-box 불러오기
+const nameBox = document.querySelector('#name-box');
+const passwordBox = document.querySelector('#password-box');
+const passwordCheckBox = document.querySelector('#password-check-box');
+const emailBox = document.querySelector('#email-box');
+const phoneNumberBox = document.querySelector('#phone-number-box');
+// console.log(phoneNumberBox);
+
+// * name Input 데이터베이스
 const members = [
   '구하림',
   '김보미',
@@ -35,10 +43,10 @@ const members = [
   '황재민',
 ];
 
-// boolean 값을 넣을 빈 배열
+// * boolean 값을 넣을 빈 배열
 const booleanArr = [];
 
-// name input의 데이터가 members 배열 값과 같은지 검사
+// * name input의 데이터가 members 배열 값과 같은지 검사
 const checkNames = (name) => {
   for (let key in members) {
     if (name === members[key]) {
@@ -48,11 +56,18 @@ const checkNames = (name) => {
   return false;
 };
 
-// root 안에 있는 input에 이벤트리스너 적용
+// * root 안에 있는 input에 이벤트리스너 적용
 root.addEventListener('change', () => {
   const data = names.value;
   let check = checkNames(data);
-  console.log(check);
-  booleanArr.push(check);
+
+  if (check === true) {
+    if (booleanArr[0] !== true) {
+      booleanArr.push(check);
+    }
+    nameBox.style.backgroundColor = '#4aff51';
+  } else {
+    nameBox.style.backgroundColor = '#ff4a4a';
+  }
   console.log(booleanArr);
 });
