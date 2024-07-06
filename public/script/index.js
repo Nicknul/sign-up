bd9b5acad9fdc7c926bf8c392c65c89f3019f167
Nicknul <jea7730@gmail.com>
2024-07-07 01:53:52 +0900

    edit_정호연 : 객체 안에 phoneNumber 항목 추가

--- public/script/index.js
+++ public/script/index.js
@@ -18,7 +18,7 @@ const emailBox = document.querySelector('#email-box');
 const phoneNumberBox = document.querySelector('#phone-number-box');
 // console.log(phoneNumberBox);
 
-// * name Input 데이터베이스
+// * 데이터베이스
 const members = [
   '구하림',
   '김보미',
@@ -42,18 +42,10 @@ const members = [
   '황재민',
 ];
 
-// * name input matching
-const checkNames = (name) => {
-  for (let key in members) {
-    if (name === members[key]) {
-      return true;
-    }
-  }
-  return false;
-};
+const number = /[0-9]/g;
 
 // * input matching object
-const test = {
+const match = {
   name: (name) => {
     for (let key in members) {
       if (name === members[key]) {
@@ -62,23 +54,59 @@ const test = {
     }
     return false;
   },
+  password: (inputData) => {},
+  phoneNumber: (inputData) => {
+    return number.test(inputData);
+  },
 };
 
 // * boolean 값을 넣을 빈 배열
 const booleanArr = [];
 
 // * root 안에 있는 input에 이벤트리스너 적용
-root.addEventListener('change', () => {
-  const data = names.value;
-  let check = test.name(data);
+root.addEventListener('change', (event) => {
+  let indexZero = booleanArr[0];
+  let name = match.name(names.value);
+  let phone = match.phoneNumber(phoneNumber.value);
+  console.log(phone);
 
-  if (check === true) {
-    if (booleanArr[0] !== true) {
-      booleanArr.push(check);
+  // ? name input
+  if (name === true) {
+    if (indexZero !== true) {
+      booleanArr.push(name);
     }
     nameBox.style.backgroundColor = '#4aff51';
   } else {
     nameBox.style.backgroundColor = '#ff4a4a';
   }
+
+  // ? phone-number input
+  if (phone === true) {
+    // true 값 배열에 넣기
+    if (indexZero === true) {
+      booleanArr.push(phone);
+    }
+  }
   console.log(booleanArr);
 });
+
+// phoneNumber.addEventListener('input', (e) => {
+//   let input = e.target.value.replace(/\D/g, '');
+
+//   // if (phone === true) {
+//   //   if (booleanArr[0] === true || booleanArr[1] !== true) {
+//   //     booleanArr.push(phone);
+//   //   }
+//   if (input.length > 3) {
+//     input = input.slice(0, 3) + '-' + input.slice(3);
+//   }
+//   if (input.length > 8) {
+//     input = input.slice(0, 8) + '-' + input.slice(8, 12);
+//     phoneNumberBox.style.backgroundColor = '#4aff51';
+//   }
+//   e.target.value = input;
+//   // } else {
+//   //   phoneNumberBox.style.backgroundColor = '#ff4a4a';
+//   // }
+//   console.log(booleanArr);
+// });
